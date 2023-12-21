6f5008d0254e8b6cbdbae8b4264479342e2bad02
Mohammed Rabil <rabil@techie.com>
2023-12-21 19:34:04 +0530

    Add Deepseek API URL constants

--- deepseek_api/constants.py
+++ deepseek_api/constants.py
@@ -1,4 +1,6 @@
 class API_URL:
+    """Deepseek API URL constants
+    """
     BASE_URL = "https://coder.deepseek.com/api/v0"
     LOGIN = BASE_URL + "/users/login"
     CLEAR_CONTEXT = BASE_URL + "/chat/clear_context"
