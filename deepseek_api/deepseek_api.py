27b8510b573b30cff3d744b1810c9cb9b38d8784
Mohammed Rabil <rabil@techie.com>
2023-12-21 19:33:38 +0530

    Refactor DeepseekAPI class constructor and add docstrings

--- deepseek_api/deepseek_api.py
+++ deepseek_api/deepseek_api.py
@@ -7,9 +7,19 @@ from deepseek_api.constants import API_URL
 from deepseek_api.errors import EmptyEmailOrPasswordError, NotLoggedInError
 
 class DeepseekAPI:
-    """A class to interact with the Deepseek API."""
+    """
+    A class to interact with the Deepseek API.
+    """
     
     def __init__(self, email: str, password: str, model_class: str = "deepseek_code", save_login: bool = False):
+        """Constructor method
+
+        Args:
+            email (str): Email of account from chat.deepseek.com
+            password (str): Password of account from chat.deepseek.com
+            model_class (str, optional): Deepseek model name "deepseek_chat" or "deepseek_code". Defaults to "deepseek_code".
+            save_login (bool, optional): Save credentials to login.json. This will prevent login function from being called multiple times in constructor. Defaults to False.
+        """
         self.email = email
         self.password = password
         self.session = requests.Session()
@@ -39,13 +49,28 @@ class DeepseekAPI:
         self.login()
         
     def __repr__(self):
+        """Representation method
+
+        Returns:
+            str: String representation of DeepseekAPI object
+        """
         return f"DeepseekAPI(email={self.email}, password={self.password}, model_class={self.model_class}, save_login={self.save_login})"
     
     def __del__(self):
+        """Destructor method
+        """
         self.session.close()
         self._scheduled_token_object.cancel()
         
     def _login(self):
+        """Login method
+
+        Raises:
+            EmptyEmailOrPasswordError: If email or password is empty
+
+        Returns:
+            dict: Login JSON response
+        """
         if self.email == "" or self.password == "":
             raise EmptyEmailOrPasswordError
         
@@ -67,6 +92,8 @@ class DeepseekAPI:
         return response.json()
     
     def login(self):
+        """Login method wrapper
+        """
         if self.save_login:
             try:
                 with open("login.json", "r") as file:
@@ -80,6 +107,8 @@ class DeepseekAPI:
         self.__schedule_update_token()
     
     def __schedule_update_token(self) -> None:
+        """Schedule the update token function as token expires every 7 days
+        """
         # Decode the JWT token
         token = self.get_token()
         decoded_token = jwt.decode(token, options={"verify_signature": False})
@@ -96,24 +125,49 @@ class DeepseekAPI:
         self._scheduled_token_object = thread_timer_obj
     
     def is_logged_in(self):
+        """Check if user is logged in
+
+        Returns:
+            bool: True if logged in, False otherwise
+        """
         if self.credentials:
             return True
         else:
             return False
         
     def raise_for_not_logged_in(self):
+        """Raise NotLoggedInError if user is not logged in
+
+        Raises:
+            NotLoggedInError: If user is not logged in
+        """
         if not self.is_logged_in():
             raise NotLoggedInError
     
     def get_credentials(self):
+        """Get credentials
+
+        Returns:
+            dict: Credentials JSON data from login response
+        """
         self.raise_for_not_logged_in()
         return self.credentials
     
     def get_token(self):
+        """Get token
+
+        Returns:
+            str: JWT Authorization token
+        """
         self.raise_for_not_logged_in()
         return self.get_credentials()["data"]["user"]["token"]
         
     def new_chat(self):
+        """Start a new chat
+
+        Returns:
+            dict: Clear context JSON response from deepseek API
+        """
         self.raise_for_not_logged_in()
         
         params = {
@@ -136,6 +190,15 @@ class DeepseekAPI:
         return response.json()
     
     def chat(self, message: str, chunk_size: int = 512):
+        """Chat with the deepseek API
+
+        Args:
+            message (str): Prompt message
+            chunk_size (int, optional): Chunk size for streaming. Defaults to 512.
+
+        Yields:
+            dict: Response JSON data from deepseek API
+        """
         self.raise_for_not_logged_in()
         
         json_data = {
