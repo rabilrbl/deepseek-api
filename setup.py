7d545384fc3c6db77b6b8ff652351d7120757026
Mohammed Rabil <rabil@techie.com>
2023-12-21 11:05:05 +0530

    Update version and add requirements.txt to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
 
+def read_requirements():
+    with open('requirements.txt') as req:
+        content = req.read()
+        requirements = content.split('\n')
+    return requirements
+
+
 setup(
     name='deepseek_api',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
+    install_requires=read_requirements(),
 )
