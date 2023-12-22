86061f99f5ebad68de6fc7ae29b5562e6cc2f21e
Mohammed Rabil <rabil@techie.com>
2023-12-22 20:10:07 +0530

    Add SyncDeepseekAPI class for synchronous interaction with Deepseek API

--- deepseek_api/deepseek_api.py
+++ deepseek_api/deepseek_api.py
@@ -257,4 +257,240 @@ class DeepseekAPI:
                     yield line
 
 
+class SyncDeepseekAPI:
+    """
+    A synchronous class to interact with the Deepseek API.
+    """
 
+    def __init__(self, email: str, password: str, model_class: str = "deepseek_code", save_login: bool = False):
+        """
+        Constructor method for DeepseekAPI class.
+
+        Initializes a DeepseekAPI instance with provided credentials and settings.
+
+        Parameters:
+        email (str): User's email for Deepseek account
+        password (str): Password for user's Deepseek account
+        model_class (str): Deepseek model to use, either 'deepseek_chat' or 'deepseek_code'
+        save_login (bool): Whether to save credentials to login.json to avoid re-login
+
+        """
+        self.email = email
+        self.password = password
+        self.model_class = model_class
+        self.save_login = save_login
+        self.headers = {
+            "Accept-Language": "en-IN,en;q=0.9",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            "DNT": "1",
+            "Origin": "https://coder.deepseek.com",
+            "Pragma": "no-cache",
+            "Referer": "https://coder.deepseek.com/",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome",
+            "accept": "*/*",
+            "content-type": "application/json",
+            "sec-ch-ua": '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Linux"',
+            "x-app-version": "20231220.2",
+        }
+
+        self.credentials = {}
+        self.session = requests.Session()
+        self.login()
+    
+    def __del__(self):
+        """Destructor method for DeepseekAPI class.
+
+        Closes the requests Session that was used for making requests to the API.
+        """
+        self.session.close()
+        
+    def _login(self):
+        """Logs in the user by sending a POST request to the login API endpoint.
+
+        Sends the login request with email, password and other required fields.
+        Saves the credentials to a file if save_login is True.
+        Returns the JSON response from the API.
+        
+        Raises:
+            EmptyEmailOrPasswordError: If the email or password is not provided.
+            requests.exceptions.RequestException: If the login request fails.
+            
+        Returns:
+            dict: Credentials JSON data from login response
+        """
+        if self.email == "" or self.password == "":
+            raise EmptyEmailOrPasswordError
+
+        json_data = {
+            "email": self.email,
+            "mobile": "",
+            "password": self.password,
+            "area_code": "",
+        }
+
+        response = self.session.post(
+            API_URL.LOGIN, headers=self.headers, json=json_data
+        )
+        self.credentials = response.json()
+        self.headers["authorization"] = "Bearer " + self.get_token()
+
+        if self.save_login:
+            with open("login.json", "w") as file:
+                file.write(json.dumps(self.credentials))
+
+        return response.json()
+    
+    def login(self):
+        """Logs the user in by loading credentials from file or calling login API.
+
+        If save_login is True, tries to load credentials from the login.json file.
+        If file not found, calls _login() to login via API.
+
+        If save_login is False, calls _login() to always login via API.
+
+        Schedules an update token callback to refresh the token periodically.
+        """
+        if self.save_login:
+            try:
+                with open("login.json", "r") as file:
+                    content = file.read()
+                    self.credentials = json.loads(content)
+
+                    self.set_authorization_header()
+            except FileNotFoundError:
+                self._login()
+        else:
+            self._login()
+            
+    # a method to call self._login if the refresh token is expired
+    def _refresh_token_if_expired(self):
+        """Refreshes the JWT token if it has expired.
+
+        Decodes the current JWT token to get the expiration time. If the token has
+        expired, calls the _login() method to refresh the token and update the
+        authorization header.
+        """
+        # Decode the JWT token
+        token = self.get_token()
+        decoded_token = jwt.decode(token, options={"verify_signature": False})
+
+        # Fetch the 'exp' value and subtract 1 hour (to be safe)
+        exp_time = datetime.datetime.fromtimestamp(
+            decoded_token["exp"]
+        ) - datetime.timedelta(hours=1)
+
+        # If the token has expired, refresh it
+        if exp_time < datetime.datetime.now():
+            self._login()
+            
+    def is_logged_in(self):
+        """Check if user is logged in
+
+        Returns:
+            bool: True if logged in, False otherwise
+        """
+        if self.credentials:
+            return True
+        else:
+            return False
+        
+    def raise_for_not_logged_in(self):
+        """Raise NotLoggedInError if user is not logged in
+
+        Raises:
+            NotLoggedInError: If user is not logged in
+        """
+        if not self.is_logged_in():
+            raise NotLoggedInError
+        
+    def set_authorization_header(self):
+        """Sets the authorization header to a JWT token.
+        
+        Gets the JWT token by calling get_token() and prepends 'Bearer ' 
+        to set the authorization header.
+        """
+        self.headers["authorization"] = "Bearer " + self.get_token()
+        
+    def get_token(self):
+        """Get token
+
+        Returns:
+            str: JWT Authorization token
+        """
+        return self.get_credentials()["data"]["user"]["token"]
+    
+    def get_credentials(self):
+        """Get credentials
+
+        Returns:
+            dict: Credentials JSON data from login response
+        """
+        return self.credentials
+    
+    def new_chat(self):
+        """Start a new chat synchronously"""
+        
+        # Check if token is expired and refresh it if needed
+        self._refresh_token_if_expired()
+        
+        params = {
+            "session_id": "1",
+        }
+        
+        json_data = {
+            'model_class': self.model_class,
+            'append_welcome_message': False,
+        }
+        
+        response = self.session.post(
+            API_URL.CLEAR_CONTEXT,
+            params=params,
+            headers=self.headers,
+            json=json_data
+        )
+        return response.json()
+    
+    def chat(self, message: str):
+        """Chat synchronously with the Deepseek API.
+
+        Sends a chat message to the Deepseek API and yields the response.
+
+        Args:
+            message (str): The chat message to send.
+
+        Yields:
+            dict: The JSON response from the API for each chat message.
+        """
+        
+        # Check if token is expired and refresh it if needed
+        self._refresh_token_if_expired()
+
+        json_data = {
+            "message": message,
+            "stream": True,
+            "model_class": self.model_class,
+            "model_preference": None,
+            "temperature": 0,
+        }
+
+        response = self.session.post(
+            API_URL.CHAT, headers=self.headers, json=json_data
+        )
+        # Check if the request was successful (status code 200)
+        response.raise_for_status()
+
+        # Iterate over the content asynchronously
+        for data in response.iter_content():
+            line = data.decode().strip().replace("data: ", "")
+            if line:
+                line = json.loads(line)
+                if line.get("payload", None) is None:
+                    line["choices"][0]["delta"]["content"] = ""
+                yield line
+                
